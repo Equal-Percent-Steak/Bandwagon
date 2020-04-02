c2401ecee6028d5b2046620e0aa775cae44d485e
hazelyn11 <hazelyna11@gmail.com>
2020-04-02 14:20:20 -0400

    beginning enter task screen

---
+++ app/src/main/java/com/equalpercentsteak/bandwagon/createTask.java
@@ -0,0 +1,14 @@
+package com.equalpercentsteak.bandwagon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task);
+    }
+}
