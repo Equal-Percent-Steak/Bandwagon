e53c3974ec744d6edc7cc0edc54a61fc314897ed
hazelyn11 <hazelyna11@gmail.com>
2020-04-06 14:23:00 -0400

    enter task box, enter date box, and dropdown class menu completed

--- app/src/main/java/com/equalpercentsteak/bandwagon/createTask.java
+++ app/src/main/java/com/equalpercentsteak/bandwagon/createTask.java
@@ -3,6 +3,8 @@ package com.equalpercentsteak.bandwagon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class createTask extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class createTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+
+        Spinner groupMenu = (Spinner) findViewById(R.id.groupChoice);
+
+        ArrayAdapter<String> groupMenuAdapter = new ArrayAdapter<String>(createTask.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(
+                        R.array.dropdownNames));
+        groupMenuAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        groupMenu.setAdapter(groupMenuAdapter);
     }
 }
