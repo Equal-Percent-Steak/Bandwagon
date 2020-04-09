41a34aa954f61f08be7caf7d2635373e534abbcb
hazelyn11 <hazelyna11@gmail.com>
2020-04-08 21:01:24 -0400

    enter task screen match parent fix, enter group screen completed

---
+++ app/src/main/java/com/equalpercentsteak/bandwagon/ui/createGroup.java
@@ -0,0 +1,16 @@
+package com.equalpercentsteak.bandwagon.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.equalpercentsteak.bandwagon.R;
+
+public class createGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+    }
+}
