7ee822bfd8c27a32e2c93fb3b4c324808ed9dc9c
hazelyn11 <hazelyna11@gmail.com>
2020-04-11 23:03:06 -0400

    create new task or group menu completed (to be opened from the home screen)

---
+++ app/src/main/java/com/equalpercentsteak/bandwagon/createNewTaskOrGroupMenu.java
@@ -0,0 +1,14 @@
+package com.equalpercentsteak.bandwagon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createNewTaskOrGroupMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_task_or_group_menu);
+    }
+}
