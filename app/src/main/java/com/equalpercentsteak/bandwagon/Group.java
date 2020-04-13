be3a1f9c46800d41dbe428130f81a0a8440631b8
kayla <krfreeman@wpi.edu>
2020-04-13 14:09:57 -0400

    Added the recyclerview list for assignments and associated classes (holder, adapter)

---
+++ app/src/main/java/com/equalpercentsteak/bandwagon/Group.java
@@ -0,0 +1,31 @@
+package com.equalpercentsteak.bandwagon;
+
+import java.util.ArrayList;
+
+public class Group {
+
+    private ArrayList<User> users;
+    private ArrayList<Assignment> assignments;
+
+    public Group(){
+        ArrayList<User> users;
+        ArrayList<Assignment> assignments;
+    }
+
+    //TODO: Andrew Firebase integration for adding user and assignments
+    public void addUser(User u){
+        users.add(u);
+    }
+
+    public void addAssignment(Assignment a){
+        assignments.add(a);
+    }
+
+    public ArrayList<User> getUsers(){
+        return users;
+    }
+
+    public ArrayList<Assignment> getAssignments(){
+        return assignments;
+    }
+}
