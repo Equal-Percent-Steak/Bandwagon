55974e8df3ba53be55b506b0c4ec4b1417eb8f3e
hazelyn11 <hazelyna11@gmail.com>
2020-04-12 23:10:21 -0400

    class settings page finished, admin option added to user class, dimension fix made to settings page

--- app/src/main/java/com/equalpercentsteak/bandwagon/User.java
+++ app/src/main/java/com/equalpercentsteak/bandwagon/User.java
@@ -3,17 +3,24 @@ package com.equalpercentsteak.bandwagon;
 public class User {
 
     private String username;
+    private boolean admin;
 
     public User(){
         username="";
+        admin=false;
     }
 
-    public User (String userName){
+    public User (String userName, boolean isAdmin){
         username = userName;
+        admin = isAdmin;
     }
 
     public String getUsername(){
         return username;
+    }
+
+    public Boolean getAdminStatus(){
+        return admin;
     }
 
     public void display(){
---
+++ app/src/main/java/com/equalpercentsteak/bandwagon/classSettings.java
@@ -0,0 +1,14 @@
+package com.equalpercentsteak.bandwagon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class classSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_settings);
+    }
+}
