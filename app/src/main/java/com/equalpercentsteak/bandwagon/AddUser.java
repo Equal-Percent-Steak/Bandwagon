a86de9ed3ec609a1c505b8de7ff5ce06a2b7a565
hazelyn11 <hazelyna11@gmail.com>
2020-05-05 20:52:48 -0400

    added spinner, fixed menu

--- app/src/main/java/com/equalpercentsteak/bandwagon/AddUser.java
+++ app/src/main/java/com/equalpercentsteak/bandwagon/AddUser.java
@@ -6,17 +6,27 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class AddUser extends AppCompatActivity {
+public class AddUser extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+
+        Spinner groupMenu = (Spinner) findViewById(R.id.groupChoice);
+
+        ArrayAdapter<String> groupMenuAdapter = new ArrayAdapter<String>(AddUser.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(
+                R.array.dropdownNames));
+        groupMenuAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        groupMenu.setAdapter(groupMenuAdapter);
     }
 
     public void onClick(View v){
